a9848ab734e435ac9400d6e82fec1a76c919af76
Evan Palmer <evansp01@gmail.com>
2013-08-01 17:11:23 -0400

    added documentation to student and coursevertex

--- leepProject/src/examScheduling/CourseVertex.java
+++ leepProject/src/examScheduling/CourseVertex.java
@@ -1,5 +1,13 @@
 package examScheduling;
 
+/**
+ * holds addition information for courses including crn, degree, weighted
+ * degree, and final block and time
+ * 
+ * @author evan
+ * 
+ */
+
 public class CourseVertex implements Comparable<CourseVertex> {
     private String name;
     private int wdegree;
@@ -7,6 +15,13 @@ public class CourseVertex implements Comparable<CourseVertex> {
     private int block;
     private int day;
 
+    /**
+     * 
+     * @param name
+     *            crn associated with course vertex
+     * @param g
+     *            graph to create vertex from
+     */
     public CourseVertex(String name, StudentGraph<String, StudentEdge> g) {
 	this.name = name;
 	wdegree = g.degreeOf(name);
@@ -15,27 +30,58 @@ public class CourseVertex implements Comparable<CourseVertex> {
 	day = -1;
     }
 
+    /**
+     * sets the final block and day of this course
+     * 
+     * @param block
+     *            final block
+     * @param day
+     *            final day
+     */
+
     public void setTime(int block, int day) {
 	this.block = block;
 	this.day = day;
     }
 
+    /**
+     * 
+     * @return returns the final day of this course -1 indicates not yet
+     *         scheduled
+     */
     public int day() {
 	return day;
     }
 
+    /**
+     * 
+     * @return returns the final block of this course -1 indicates not yet
+     *         scheduled
+     */
     public int block() {
 	return block;
     }
 
+    /**
+     * 
+     * @return returns the crn associated with this vertex
+     */
     public String name() {
 	return name;
     }
 
+    /**
+     * 
+     * @return return the unweighted degree of this course vertex
+     */
     public int getDegree() {
 	return degree;
     }
 
+    /**
+     * 
+     * @return returns the weighted degree of this course vertex
+     */
     public int getWeightedDegree() {
 	return wdegree;
     }
--- leepProject/src/examScheduling/Student.java
+++ leepProject/src/examScheduling/Student.java
@@ -2,34 +2,81 @@ package examScheduling;
 
 import java.util.Arrays;
 
+/**
+ * Student class holds additional information about students -- their student idF and
+ * when they have exams
+ * 
+ * @author evan
+ * 
+ */
+
 public class Student {
 
     private String name = null;
     private boolean[][] blocks;
 
+    /**
+     * 
+     * @param name
+     *            student id number
+     * @param days
+     *            the number of final days currently being used
+     * @param blocksPerDay
+     *            the number of blocks in each day
+     */
     public Student(String name, int days, int blocksPerDay) {
 	this.name = name;
 	blocks = new boolean[days][blocksPerDay];
     }
 
+    /**
+     * 
+     * @return returns the student id associated with this student
+     */
     public String name() {
 	return name;
     }
 
+    /**
+     * resets the occupancy of this student -- sets all blocks to unoccupied
+     */
     public void resetOccupancy() {
 	for (boolean[] i : blocks) {
 	    Arrays.fill(i, false);
 	}
     }
 
+    /**
+     * sets a block to occupied
+     * 
+     * @param day
+     *            the day of the block
+     * @param block
+     *            the block number
+     */
     public void occupy(int day, int block) {
 	blocks[day][block] = true;
     }
 
+    /**
+     * 
+     * @param day
+     *            the day of the block to check
+     * @param block
+     *            the block number
+     * @return returns true if the block is occupied, false otherwise
+     */
     public boolean isOccupied(int day, int block) {
 	return blocks[day][block];
     }
 
+    /**
+     * counts the number of exams in a day
+     * 
+     * @param day
+     *            the day to count exams of
+     * @return the number of exams in that day
+     */
     public int examsInDay(int day) {
 	int exams = 0;
 	for (boolean b : blocks[day]) {
@@ -39,6 +86,15 @@ public class Student {
 	return exams;
     }
 
+    /**
+     * returns true if there are N exams or more in the given day
+     * 
+     * @param N
+     *            the threshold number of exams
+     * @param day
+     *            the day
+     * @return returns true if examsInDay() >= N
+     */
     public boolean gtNExamsInDay(int N, int day) {
 	return examsInDay(day) >= N;
     }
